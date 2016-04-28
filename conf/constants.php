9d948dab797bac9b2584aefaa7829fe799dad78f
Suraj Kumar Jana <surajjana2@gmail.com>
2016-04-28 16:07:03 +0530

    new commit

--- conf/constants.php
+++ conf/constants.php
@@ -1,7 +1,7 @@
 <?php  
 
-define('HOST','http://localhost/savyrocket/');
+define('HOST','http://localhost/flyingbook/');
 
-/*define('HOST','http://52.34.226.223/savyrocket/')*/
+/*define('HOST','http://52.34.226.223/flyingbook/')*/
 
 ?>
