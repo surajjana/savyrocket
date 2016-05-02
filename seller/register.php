fbbebac29032f2f32155ce6ed93e1171991a0b4c
Suraj Kumar Jana <surajjana2@gmail.com>
2016-05-03 00:37:20 +0530

    new commit

--- seller/register.php
+++ seller/register.php
@@ -22,6 +22,22 @@ if(! $retval )
 
 mysql_close($conn);
 
+$to = $_POST['email'];
+$subject = 'Welcome To Flying Book';
+
+$message = "<html><body>
+                <p>Welcome ".$_POST['sname']."!!</p>
+                <p>We are glad to have you on Flying Book.</p><br /><br />
+                <p>Thank you,</p>
+                <p>Flying Book Team</p>
+                ";
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+                $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+                $headers .= 'From: Flying Book <noreply@flyingbook.com>' . "\r\n";
+
+mail($to,$subject,$message,$headers);
+
 echo '<center><h2>Registration Complete</h2><br /><a href="index.html">Click Here To Log In</a></center>';
 
 ?>
