d85f21136f36914bf1df4d71a8da5a56e08e760b
Suraj Jana <surajjana2@gmail.com>
2016-06-08 17:39:14 +0530

    new commit

---
+++ bid_it.php
@@ -0,0 +1,25 @@
+<?php  
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+require_once("conf/constants.php");
+session_start();
+
+$conn = mysql_connect(HOST, USER, PASSWORD);
+if(! $conn )
+{
+  die('Could not connect: ' . mysql_error());
+}
+
+mysql_select_db(DB);
+
+$sql = "select * from books where book_id=".$_GET['id'];
+$retval = mysql_query( $sql, $conn );
+
+if(! $retval )
+{
+  die('Could not get data: ' . mysql_error());
+}
+
+var_dump($retval);
+
+?>
