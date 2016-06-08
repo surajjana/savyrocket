2ae66ea3fbf0a0984d69517fa02450fcf0727ac4
root <root@ip-172-31-29-81.us-west-2.compute.internal>
2016-06-08 05:14:59 +0000

    new commit

--- conf/constants.php
+++ conf/constants.php
@@ -1,12 +1,12 @@
 <?php  
 
-define('DOMAIN','http://localhost/flyingbook/');
-define('DOMAIN_SELLER','http://localhost/flyingbook/seller/');
+//define('DOMAIN','http://localhost/flyingbook/');
+//define('DOMAIN_SELLER','http://localhost/flyingbook/seller/');
 define("HOST", "localhost");
 define("USER", "root");
 define("PASSWORD","hack123");
 define("DB", "flyingbook");
 
-/*define('HOST','http://52.34.226.223/flyingbook/')*/
-
+define('DOMAIN','http://52.34.226.223/flyingbook/');
+define('DOMAIN_SELLER','http://52.34.226.223/flyingbook/seller/');
 ?>
