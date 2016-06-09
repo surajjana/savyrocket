e2567f33d20cfccdfeba3f6eb91fbeec49ba6709
Suraj Jana <surajjana2@gmail.com>
2016-06-09 11:03:45 +0530

    new commit

--- home.php
+++ home.php
@@ -247,7 +247,7 @@ if(! $retval_bid )
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Trending Biding</h2>
+                    <h2 class="section-heading">Trending Bidding</h2>
                     <hr class="primary">
                 </div>
             </div>
